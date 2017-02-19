d7eb347aafe85eeff01bb74e2986380e819730c2
unknown <daumisgod@gmail.com>
2017-02-19 18:31:58 -0500

    finished exercise 3

--- 03-types-and-typeclasses.hs
+++ 03-types-and-typeclasses.hs
@@ -19,10 +19,12 @@ data Colour = Red | Orange | Yellow | Green | Blue | Indigo | Violet
  - The Colour typeclass is of type Ord
  - What is the "first" (or least) colour
  -}
-firstColour = undefined
+firstColour :: Colour --firstColour rets a Colour
+firstColour = minBound :: Colour --b/c Colour is Ord and Bounded, you can use minBound to get Least color
 
 -- List the colours in reverse order
-reverseColourOrder = undefined
+reverseColourOrder :: [Colour] --gives back list of Colours
+reverseColourOrder = reverse[minBound :: Colour .. maxBound :: Colour] --call reverse on whole colour range
 
 {-
  - Mix two colours together, to produce the average value of the two.
@@ -31,4 +33,5 @@ reverseColourOrder = undefined
  - For example: paintMix Green Violet = Indigo
  - Hint: Integer division can be performed with the quot function: quot 7 2 = 3
  -}
-paintMix c1 c2 = undefined
+paintMix :: Colour -> Colour -> Colour -- take in two colours output colour
+paintMix c1 c2 = [c1 .. c2] !! quot (length [c1 .. c2]) 2 --grab the element out of Colour's data of index average of the two indexes of Colours passed in
